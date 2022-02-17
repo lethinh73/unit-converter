abfc7ed7975979c9f889b728f6a55cc8e8050eef
Thinh Le <lethinh73@gmail.com>
2022-02-17 14:19:01 -0600

    added team logo

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-import functools
 import tkinter
 from functions import *
 from PIL import Image, ImageTk
 
 # Create the main window
 theApp = tkinter.Tk()
-canvas = tkinter.Canvas(theApp, width=800, height=500)
+canvas = tkinter.Canvas(theApp, width=800, height=500, bg=('#470D21'))
 canvas.grid(columnspan=3)
+
+# Insert team logo at the corner of the window
+teamLogo = Image.open("logo.png")
+teamLogo = teamLogo.resize((100, 30), Image.ANTIALIAS)
+teamLogo = ImageTk.PhotoImage(teamLogo)
+canvas.create_image(750, 485, image=teamLogo)
+
 
 # Main Application Loop
 theApp.mainloop()
