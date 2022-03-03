799fd6f650a65cd1998cd99a832c2b617a3cd591
Thinh Le <lethinh73@gmail.com>
2022-03-02 18:12:26 -0600

    created and updated 2 tabs

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
+from turtle import bgcolor
+from black import main
+from click import style
 from functions import *
-from tkinter import *
+from tkinter import Label, ttk, Tk
 
 mainApp = Tk()
+tabControl = ttk.Notebook(mainApp)
 
 # Set title and size for the window
 mainApp.title("Team Speak - Unit Converter/Calculator")
 mainApp.geometry("500x650")
 
-# App background
-mainApp.configure(background="#94B49F")
-
 # App title
-appTitle = Label(
-    mainApp, text="Unit Converter and Calculator", fg="#362706", bg="#94B49F"
-)
+appTitle = Label(mainApp, text="Unit Converter and Calculator", fg="white")
 appTitle.config(font=("Arial", 30))
 appTitle.pack(pady=20)
+
+# Create 2 tabs
+tabConverter = ttk.Frame(tabControl)
+tabCalculator = ttk.Frame(tabControl)
+tabControl.add(tabConverter, text="Number Converting")
+tabControl.add(tabCalculator, text="Calculator")
+tabControl.pack(expand=1, fill="both")
+
+# Tab Converter
+text = Label(tabConverter, text="Hello", fg="white")
+text.pack(side="left", ipadx=10)
+# Tab Calculator
+
 mainApp.mainloop()
